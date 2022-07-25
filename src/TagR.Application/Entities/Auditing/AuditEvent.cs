7de220492991aa533abc7581513a9629c3170320
Jeremy <jenkenkur@gmail.com>
2022-07-26 00:40:01 +0200

    Create AuditEvent base record and specific event implementations

---
+++ src/TagR.Application/Entities/Auditing/AuditEvent.cs
@@ -0,0 +1,20 @@
+﻿using Remora.Rest.Core;
+using TagR.Domain;
+
+namespace TagR.Application.Entities.Auditing;
+
+public record AuditEvent
+{
+    public TagAuditLogAction AuditAction { get; }
+
+    public int TagId { get; }
+
+    public Snowflake Actor { get; }
+
+    public AuditEvent(TagAuditLogAction auditAction, int tagId, Snowflake actor)
+    {
+        AuditAction = auditAction;
+        TagId = tagId;
+        Actor = actor;
+    }
+}
