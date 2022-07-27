25a33ecc69ee37de61f228a7ac11eececda4efb1
Jeremy <jenkenkur@gmail.com>
2022-07-28 00:45:37 +0200

    Fetch command groups, check if the tag is disabled and increment tag use
    * If the given tag parameter starts with the name of one of the command groups then it will return early.
    * If a tag is disabled it will return early.
    * If a tag is invoked and used, usage is increased on the Tag object and appropriately saved in the DB

--- src/TagR.Application/Services/MessageProcessingService.cs
+++ src/TagR.Application/Services/MessageProcessingService.cs
@@ -1,4 +1,6 @@
-﻿using Remora.Rest.Core;
+﻿using Remora.Commands.Services;
+using Remora.Commands.Trees.Nodes;
+using Remora.Rest.Core;
 using TagR.Application.Services.Abstractions;
 
 namespace TagR.Application.Services;
@@ -9,30 +11,56 @@ public class MessageProcessingService : IMessageProcessingService
     private readonly IDiscordMessageService _messageService;
     private readonly ILogger<MessageProcessingService> _logger;
 
-    public MessageProcessingService(ITagService tagService, IDiscordMessageService messageService, ILogger<MessageProcessingService> logger)
+    private readonly IReadOnlyList<IChildNode> _commandGroupNames;
+
+    public MessageProcessingService
+    (
+        ITagService tagService,
+        IDiscordMessageService messageService,
+        CommandService commandService,
+        ILogger<MessageProcessingService> logger
+    )
     {
         _tagService = tagService;
         _messageService = messageService;
         _logger = logger;
+
+        _commandGroupNames = GetCommandGroups(commandService);
     }
 
     public async Task ProcessMessageAsync(Snowflake channelId, Snowflake messageId, string messageContent, CancellationToken ct = default)
     {
-        _logger.LogInformation("Start processing message id: {messageId} ", messageId);
-        
+        _logger.LogInformation("Processing message id: {messageId} ", messageId);
+
         var content = StripPrefix(messageContent);
+
+        if (_commandGroupNames.Any(c => content.StartsWith(c.Key)))
+            return;
 
         var getTag = await _tagService.GetTagByName(content);
 
-        var tagDefined = getTag.IsDefined(out var tag);
-        await _messageService.CreateMessageAsync(channelId, tagDefined ? tag!.Content : TagNotFoundText, ct);
-        
-        _logger.LogInformation("End processing message id: {messageId} ", messageId);
+        if (!getTag.IsDefined(out var tag))
+        {
+            await _messageService.CreateMessageAsync(channelId, TagNotFoundText, ct);
+        }
+
+        if (tag!.Disabled)
+            return;
+
+        await _messageService.CreateMessageAsync(channelId, tag!.Content, ct);
+        await _tagService.IncrementTagUseAsync(tag, ct);
     }
 
     private string StripPrefix(string messageContent)
     {
         return messageContent[1..];
+    }
+
+    private IReadOnlyList<IChildNode> GetCommandGroups(CommandService cmdService)
+    {
+        return cmdService.TreeAccessor.TryGetNamedTree(null, out var tree)
+            ? tree.Root.Children
+            : throw new InvalidOperationException("Couldn't retrieve command tree.");
     }
 
     private const string TagNotFoundText = "Tag not found";
